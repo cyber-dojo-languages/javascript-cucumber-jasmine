72bc17fa13115bb5fcbcacc1c7e9b303d185b450
Jon Jagger <jon@jaggersoft.com>
2018-03-31 09:20:40 +0100

    upgrade to node 9.10.1

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,7 +1,10 @@
 # Change the version of Node.js to use, see https://nodejs.org
-# to see the supported ES6 features see: https://kangax.github.io/compat-table/es6/
-# 4.1.1 supports some ES6 (about 50%) features: https://nodejs.org/en/docs/es6/
-# 0.12.7 is the latest version without most ES6 (about 20%) features: https://nodejs.org/docs/latest-v0.12.x/api/
+# to see the supported ES6 features see:
+# https://kangax.github.io/compat-table/es6/
+# 4.1.1 supports some ES6 (about 50%)
+# features: https://nodejs.org/en/docs/es6/
+# 0.12.7 is the latest version without most ES6 (about 20%)
+# features: https://nodejs.org/docs/latest-v0.12.x/api/
 #
 # set the version to use:
 #NODE_VERSION=0.12.7
@@ -10,7 +13,8 @@
 #NODE_VERSION=6.11.1
 #NODE_VERSION=8.2.1
 #NODE_VERSION=8.4.0
-NODE_VERSION=9.4.0
+#NODE_VERSION=9.4.0
+NODE_VERSION=9.10.1
 
 # You have to cd to a specific folder because
 #   ~/node_modules/cucumber/bin/cucumber.js
