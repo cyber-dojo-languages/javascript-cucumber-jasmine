e0a6d23ff449d3fa751a5c4c1e0060b0cf5d543c
Jon Jagger <jon@jaggersoft.com>
2018-12-10 06:34:56 +0100

    prevent spurious node warning

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -17,6 +17,12 @@
 #NODE_VERSION=9.10.1
 NODE_VERSION=10.1.0
 
+# Turn off node warnings to get rid of
+# (node:28) ExperimentalWarning: The fs.promises API is experimental
+# which curently causes cyber-dojo to load stderr instead of
+# stdout when green
+export NODE_NO_WARNINGS=1
+
 if [ -f .jshintrc ]; then
   n use $NODE_VERSION \
     /usr/lib/node_modules/jshint/bin/jshint \
