0a8599389983db987ca50883768796a7809bba0f
Jon Jagger <jon@jaggersoft.com>
2020-05-01 19:06:33 +0100

    Move to node 14.1.0

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,11 +1,3 @@
-# Change the version of Node.js to use, see https://nodejs.org
-# to see the supported ES6 features see:
-# https://kangax.github.io/compat-table/es6/
-# 4.1.1 supports some ES6 (about 50%)
-# features: https://nodejs.org/en/docs/es6/
-# 0.12.7 is the latest version without most ES6 (about 20%)
-# features: https://nodejs.org/docs/latest-v0.12.x/api/
-#
 # set the version to use:
 #NODE_VERSION=0.12.7
 #NODE_VERSION=4.1.1
@@ -16,13 +8,8 @@
 #NODE_VERSION=9.4.0
 #NODE_VERSION=9.10.1
 #NODE_VERSION=10.1.0
-NODE_VERSION=13.7.0
-
-# Turn off node warnings to get rid of
-# (node:28) ExperimentalWarning: The fs.promises API is experimental
-# which curently causes cyber-dojo to load stderr instead of
-# stdout when green
-export NODE_NO_WARNINGS=1
+#NODE_VERSION=13.7.0
+NODE_VERSION=14.1.0
 
 if [ -f .jshintrc ]; then
   n use $NODE_VERSION \
